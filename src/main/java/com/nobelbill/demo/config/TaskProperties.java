e114a2e235b7ed7d308a288118544a212a6b15b0
nobelbill <nobelbill@nobelbillui-MacBookPro.local>
2021-01-23 14:24:18 +0900

    init

--- src/main/java/com/nobelbill/demo/config/TaskProperties.java
+++ src/main/java/com/nobelbill/demo/config/TaskProperties.java
@@ -1,2 +1,6 @@
-package com.nobelbill.demo.config;public class TaskProperties {
+package com.nobelbill.demo.config;
+
+
+public class TaskProperties {
+    private String url = "www.naver.com";
 }
---
+++ src/main/java/com/nobelbill/demo/config/TaskRunner.java
@@ -0,0 +1,31 @@
+package com.nobelbill.demo.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.cloud.task.listener.TaskExecutionListener;
+import org.springframework.cloud.task.repository.TaskExecution;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableTask
+public class TaskRunner implements CommandLineRunner , TaskExecutionListener {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Runner...");
+    }
+
+    @Override
+    public void onTaskStartup(TaskExecution taskExecution) {
+
+    }
+
+    @Override
+    public void onTaskEnd(TaskExecution taskExecution) {
+
+    }
+
+    @Override
+    public void onTaskFailed(TaskExecution taskExecution, Throwable throwable) {
+
+    }
+}
